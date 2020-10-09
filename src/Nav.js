d7876df0b6677c9ea54a6185837aa8e9fddf44de
Paramjeet Dhiman <paramjeetdhiman1997@gmail.com>
2020-10-09 18:16:43 +0530

    add navbar

--- src/Nav.js
+++ src/Nav.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import './Nav.css';
 
 const Nav = () => {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+
+    return () => {
+      //cleaning ...
+      window.removeEventListener('scroll');
+    };
+  }, []);
+
   return (
-    <div className="nav">
+    <div className={`nav ${show && 'nav__black'}`}>
       <img
+        className="nav__logo"
         src="https://brand.netflix.com/static/assets/icons/netflix_logo.svg"
         alt=""
       />
 
       <img
+        className="nav__avatar"
         src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png"
         alt=""
       />
